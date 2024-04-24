5b10c3aa4dd563fe153fc5b6dad9ace99f002667
HCHHOH <39873837+HCHHOH@users.noreply.github.com>
2024-04-24 15:33:37 +0800

    Add files via upload

---
+++ dataset_prepare.py
@@ -0,0 +1,294 @@
+import os
+import numpy as np
+
+# data config
+adj_filename = "data/PEMS08/PEMS08.csv"
+graph_signal_matrix_filename = "data/PEMS08/PEMS08.npz"
+num_of_vertices = 170
+points_per_hour = 12
+num_for_predict = 12
+len_input = 12
+dataset_name = "PEMS08"
+
+#training config
+num_of_weeks = 0 # 历史数据选择，以周为单位，num=3: [(0, 12), (2016, 2028), (4032, 4044)]
+num_of_days = 0 # 历史数据范围，以天为单位
+num_of_hours = 1
+
+data = np.load(graph_signal_matrix_filename)
+print(data['data'].shape)
+
+# 生成一系列索引范围 (start_idx, end_idx)
+def search_data(sequence_length, num_of_depend, label_start_idx,
+                num_for_predict, units, points_per_hour):
+    '''
+    Parameters
+    ----------
+    sequence_length: int, length of all history data
+    num_of_depend: int,
+    label_start_idx: int, the first index of predicting target
+    num_for_predict: int, the number of points will be predicted for each sample
+    units: int, week: 7 * 24, day: 24, recent(hour): 1
+    points_per_hour: int, number of points per hour, depends on data
+    Returns
+    ----------
+    list[(start_idx, end_idx)]
+    '''
+
+    if points_per_hour < 0:
+        raise ValueError("points_per_hour should be greater than 0!")
+
+    if label_start_idx + num_for_predict > sequence_length:
+        return None
+
+    x_idx = []
+    for i in range(1, num_of_depend + 1): # num_of_depend: num_of_weeks, num_of_days
+        start_idx = label_start_idx - points_per_hour * units * i
+        end_idx = start_idx + num_for_predict
+        if start_idx >= 0:
+            x_idx.append((start_idx, end_idx))
+        else:
+            return None
+
+    if len(x_idx) != num_of_depend:
+        return None
+
+    return x_idx[::-1] # 返回一个逆序的列表，按照时间顺序反向处理时间窗口
+
+def get_sample_indices(data_sequence, num_of_weeks, num_of_days, num_of_hours,
+                       label_start_idx, num_for_predict, points_per_hour=12):
+    '''
+    Parameters
+    ----------
+    data_sequence: np.ndarray
+                   shape is (sequence_length, num_of_vertices, num_of_features)
+    num_of_weeks, num_of_days, num_of_hours: int
+    label_start_idx: int, the first index of predicting target, 预测值开始的那个点
+    num_for_predict: int, the number of points will be predicted for each sample
+    points_per_hour: int, default 12, number of points per hour
+    Returns
+    ----------
+    week_sample: np.ndarray
+                 shape is (num_of_weeks * points_per_hour,
+                           num_of_vertices, num_of_features)
+    day_sample: np.ndarray
+                 shape is (num_of_days * points_per_hour,
+                           num_of_vertices, num_of_features)
+    hour_sample: np.ndarray
+                 shape is (num_of_hours * points_per_hour,
+                           num_of_vertices, num_of_features)
+    target: np.ndarray
+            shape is (num_for_predict, num_of_vertices, num_of_features)
+    '''
+    week_sample, day_sample, hour_sample = None, None, None
+
+    if label_start_idx + num_for_predict > data_sequence.shape[0]:
+        return week_sample, day_sample, hour_sample, None
+
+    if num_of_weeks > 0:
+        week_indices = search_data(data_sequence.shape[0], num_of_weeks,
+                                   label_start_idx, num_for_predict,
+                                   7 * 24, points_per_hour)
+        if not week_indices:
+            return None, None, None, None
+
+        week_sample = np.concatenate([data_sequence[i: j]
+                                      for i, j in week_indices], axis=0)
+
+    if num_of_days > 0:
+        day_indices = search_data(data_sequence.shape[0], num_of_days,
+                                  label_start_idx, num_for_predict,
+                                  24, points_per_hour)
+        if not day_indices:
+            return None, None, None, None
+
+        day_sample = np.concatenate([data_sequence[i: j]
+                                     for i, j in day_indices], axis=0)
+
+    if num_of_hours > 0:
+        hour_indices = search_data(data_sequence.shape[0], num_of_hours,
+                                   label_start_idx, num_for_predict,
+                                   1, points_per_hour)
+        if not hour_indices:
+            return None, None, None, None
+
+        hour_sample = np.concatenate([data_sequence[i: j]
+                                      for i, j in hour_indices], axis=0)
+
+    target = data_sequence[label_start_idx: label_start_idx + num_for_predict]
+
+    return week_sample, day_sample, hour_sample, target
+
+def MinMaxnormalization(train, val, test):
+    '''
+    Parameters
+    ----------
+    train, val, test: np.ndarray (B,N,F,T)
+    Returns
+    ----------
+    stats: dict, two keys: mean and std
+    train_norm, val_norm, test_norm: np.ndarray,
+                                     shape is the same as original
+    '''
+
+    assert train.shape[1:] == val.shape[1:] and val.shape[1:] == test.shape[1:]  # ensure the num of nodes is the same
+
+    _max = train.max(axis=(0, 1, 3), keepdims=True)
+    _min = train.min(axis=(0, 1, 3), keepdims=True)
+
+    print('_max.shape:', _max.shape)
+    print('_min.shape:', _min.shape)
+
+    def normalize(x):
+        x = 1. * (x - _min) / (_max - _min) # 归一化：所有值都在 [0, 1] 的范围内。
+        x = 2. * x - 1. # 线性变换：将数据映射到 [-1, 1] 的范围
+        return x
+
+    train_norm = normalize(train)
+    val_norm = normalize(val)
+    test_norm = normalize(test)
+
+    return {'_max': _max, '_min': _min}, train_norm, val_norm, test_norm
+
+def read_and_generate_dataset_encoder_decoder(graph_signal_matrix_filename,
+                                              num_of_weeks, num_of_days,
+                                              num_of_hours, num_for_predict,
+                                              points_per_hour=12, save=False):
+    '''
+    Parameters
+    ----------
+    graph_signal_matrix_filename: str, path of graph signal matrix file
+    num_of_weeks, num_of_days, num_of_hours: int
+    num_for_predict: int
+    points_per_hour: int, default 12, depends on data
+
+    Returns
+    ----------
+    feature: np.ndarray,
+             shape is (num_of_samples, num_of_depend * points_per_hour,
+                       num_of_vertices, num_of_features)
+    target: np.ndarray,
+            shape is (num_of_samples, num_of_vertices, num_for_predict)
+    '''
+
+    data_seq = np.load(graph_signal_matrix_filename)['data']  # (16992, 307, 1)
+
+    all_samples = []
+    for idx in range(data_seq.shape[0]):
+        sample = get_sample_indices(data_seq, num_of_weeks, num_of_days,
+                                    num_of_hours, idx, num_for_predict,
+                                    points_per_hour)
+        if ((sample[0] is None) and (sample[1] is None) and (sample[2] is None)):
+            continue
+
+        week_sample, day_sample, hour_sample, target = sample
+
+        sample = []  # [(week_sample),(day_sample),(hour_sample),target,time_sample]
+
+        if num_of_weeks > 0:
+            week_sample = np.expand_dims(week_sample, axis=0).transpose((0, 2, 3, 1))  # (T,N,F)-->(1,N,F,T)
+            sample.append(week_sample)
+
+        if num_of_days > 0:
+            day_sample = np.expand_dims(day_sample, axis=0).transpose((0, 2, 3, 1))  # (1,N,F,T)
+            sample.append(day_sample)
+
+        if num_of_hours > 0:
+            hour_sample = np.expand_dims(hour_sample, axis=0).transpose((0, 2, 3, 1))  # (1,N,F,T)
+            sample.append(hour_sample)
+
+        target = np.expand_dims(target, axis=0).transpose((0, 2, 3, 1))[:, :, 0, :]  # (1,N,T) 只取flow特征
+        sample.append(target)
+
+        time_sample = np.expand_dims(np.array([idx]), axis=0)  # (1,1)，标识其他sample是第几个时间步？
+        sample.append(time_sample)
+
+        # sampe：[(week_sample),(day_sample),(hour_sample),target,time_sample] = [(1,N,F,Tw),(1,N,F,Td),(1,N,F,Th),(1,N,Tpre),(1,1)]
+        all_samples.append(sample)
+
+    split_line1 = int(len(all_samples) * 0.6)
+    split_line2 = int(len(all_samples) * 0.8)
+
+    # 使用 zip 函数将这些元素的相应位置的元组打包。这样，每个元组的第一个元素组成一个新的元组，每个元组的第二个元素组成一个新的元组，依此类推。
+    # np.concatenate 操作，对上一步中得到的每个打包后的元组，将它们沿着第一个轴（行）连接。
+    training_set = [np.concatenate(i, axis=0)
+                    for i in zip(*all_samples[:split_line1])]  # [(B,N,F,Tw),(B,N,F,Td),(B,N,F,Th),(B,N,Tpre),(B,1)]
+    validation_set = [np.concatenate(i, axis=0)
+                      for i in zip(*all_samples[split_line1: split_line2])]
+    testing_set = [np.concatenate(i, axis=0)
+                   for i in zip(*all_samples[split_line2:])]
+
+    # (B,N,F,T'), concat multiple time series segments (for week, day, hour) together
+    train_x = np.concatenate(training_set[:-2], axis=-1)
+    val_x = np.concatenate(validation_set[:-2], axis=-1)
+    test_x = np.concatenate(testing_set[:-2], axis=-1)
+
+    train_target = training_set[-2]  # (B,N,T)
+    val_target = validation_set[-2]
+    test_target = testing_set[-2]
+
+    train_timestamp = training_set[-1]  # (B,1)
+    val_timestamp = validation_set[-1]
+    test_timestamp = testing_set[-1]
+
+    # max-min normalization on x
+    (stats, train_x_norm, val_x_norm, test_x_norm) = MinMaxnormalization(train_x, val_x, test_x)
+
+    all_data = {
+        'train': {
+            'x': train_x_norm,
+            'target': train_target,
+            'timestamp': train_timestamp,
+        },
+        'val': {
+            'x': val_x_norm,
+            'target': val_target,
+            'timestamp': val_timestamp,
+        },
+        'test': {
+            'x': test_x_norm,
+            'target': test_target,
+            'timestamp': test_timestamp,
+        },
+        'stats': {
+            '_max': stats['_max'],
+            '_min': stats['_min'],
+        }
+    }
+    print('train x:', all_data['train']['x'].shape)
+    print('train target:', all_data['train']['target'].shape)
+    print('train timestamp:', all_data['train']['timestamp'].shape)
+    print()
+    print('val x:', all_data['val']['x'].shape)
+    print('val target:', all_data['val']['target'].shape)
+    print('val timestamp:', all_data['val']['timestamp'].shape)
+    print()
+    print('test x:', all_data['test']['x'].shape)
+    print('test target:', all_data['test']['target'].shape)
+    print('test timestamp:', all_data['test']['timestamp'].shape)
+    print()
+    print('train data max :', stats['_max'].shape, stats['_max'])
+    print('train data min :', stats['_min'].shape, stats['_min'])
+
+    if save:
+        file = os.path.basename(graph_signal_matrix_filename).split('.')[0]
+        dirpath = os.path.dirname(graph_signal_matrix_filename)
+        filename = os.path.join(dirpath,
+                                file + '_r' + str(num_of_hours) + '_d' + str(num_of_days) + '_w' + str(num_of_weeks))
+        print('save file:', filename)
+        np.savez_compressed(filename,
+                            train_x=all_data['train']['x'], train_target=all_data['train']['target'],
+                            train_timestamp=all_data['train']['timestamp'],
+                            val_x=all_data['val']['x'], val_target=all_data['val']['target'],
+                            val_timestamp=all_data['val']['timestamp'],
+                            test_x=all_data['test']['x'], test_target=all_data['test']['target'],
+                            test_timestamp=all_data['test']['timestamp'],
+                            mean=all_data['stats']['_max'], std=all_data['stats']['_min']
+                            )
+    return all_data
+
+
+all_data = read_and_generate_dataset_encoder_decoder(graph_signal_matrix_filename,
+                                                     num_of_weeks, num_of_days, num_of_hours,
+                                                     num_for_predict, points_per_hour=points_per_hour,
+                                                     save=True)
